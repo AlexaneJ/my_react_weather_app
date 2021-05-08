986f4c537c07e8d59ef86efedff2d606bd90916a
Alexane J <alexanetouti@hotmail.Fr>
2021-05-08 20:46:21 +0200

    further steps : some css

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="New York" />
+        <footer>
           <a
             href="https://github.com/AlexaneJ/my_react_weather_app"
             target="_blank"
             rel="noopener noreferrer"
+            className = "ref"
           >
            Open source code
           </a>
           {" "}  by Alexane Jouglar
+          </footer>
       </div>
     </div>
   );
